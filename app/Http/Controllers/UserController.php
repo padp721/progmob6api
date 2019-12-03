0761fbb2f320714cc67648794f76802178c6edb7
GusadeTaruna <gusadetaruna04@gmail.com>
2019-12-03 21:58:07 +0800

    edit karyawan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -147,7 +147,7 @@ class UserController extends Controller
     public function updateKaryawan(Request $request, $id)
     {
 
-        $user = User::find($id);
+        $user = User::where($request->input('email'), '=' ,$user->email)->find($id);
         $password = $request->input('password');
         if (!Hash::check($password, $user->password)) {
             return response()->json(['success'=>false, 'message' => 'Password salah, coba cek lagi']);
@@ -157,7 +157,7 @@ class UserController extends Controller
             $user->save();
             return response()->json(['success'=>true,'message'=>'success', 'data' => $user]);
         }
-        
+
        // User::where('id',$id)->update($request->except('_token','_method')); 
        // return response()->json(['error'=> FALSE], $this->successStatus);
     }
