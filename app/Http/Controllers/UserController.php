f6f8520c34aa64f1ce556f0eabec15321e1a1455
Tewetewe <trywiguna@gmail.com>
2019-12-30 11:45:13 +0800

    save token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,6 +51,7 @@ class UserController extends Controller
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $user->fcm_token = request('fcm_token');
+            $user->save();
             $user['token'] =  $user->createToken('nApp')->accessToken;
             return response()->json(['error' => FALSE, 'user' => $user], $this->successStatus);
         }
