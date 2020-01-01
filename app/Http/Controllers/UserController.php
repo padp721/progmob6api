edb7dd9af4eaabf4421bfb3b9236542350729ac6
randomize721 <anggadp91@gmail.com>
2020-01-01 16:14:18 +0800

    updated user detail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,7 +69,8 @@ class UserController extends Controller
         $uang = Simpanan::select(DB::raw('SUM(nominal_transaksi) as saldo'))
             ->where('id_user_nasabah',$userDetail->id)
             ->first();
-        array_merge($userDetail, $uang);
+        array_merge((array)$userDetail, $uang);
+        $userDetail = (object)$userDetail;
         return response()->json($userDetail, $this->successStatus);
     }
     //LOGIN=================
